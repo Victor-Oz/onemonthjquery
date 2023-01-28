ce4b5919ca35fb39666936f98591d2f55e996528
Victor Oz <victorozurumba12@gmail.com>
2023-01-28 19:07:58 +0100

    fixed  issues

--- project-3/index.js
+++ project-3/index.js
@@ -1,10 +1,51 @@
 /** @format */
-
 $(document).ready(function () {
-  // YOUR CODE HERE!
+  $("#list-items").html(localStorage.getItem("listItems"));
+
   $(".add-items").submit(function (event) {
     event.preventDefault();
-   let item = $("#todo-list-item").val();
-   $("#list-items").append("<li><input type='checkbox' />" + item + "<a class='remove' >x</a></li><hr>")
+
+    var item = $("#todo-list-item").val();
+    if (item === "ye") {
+      $("#list-items").append(
+        "<li><input class='checkbox' type='checkbox'/>" +
+        "  <img id='ye' class='ye' src='1.gif' /> "+
+          "<a class='remove'>x</a><hr></li>"
+      );
+      localStorage.setItem("listItems", $("#list-items").html());
+      $("#todo-list-item").val("");
+    } else if (item) {
+      $("#list-items").append(
+        "<li><input class='checkbox' type='checkbox'/>" +
+          item +
+          "<a class='remove'>x</a><hr></li>"
+      );
+      localStorage.setItem("listItems", $("#list-items").html());
+      $("#todo-list-item").val("");
+    }
+  });
+
+  // $(".checkbox").change(function() {
+  // console.log("Checkbox checked!");
+  // })
+
+  $(document).on("change", ".checkbox", function () {
+    if ($(this).attr("checked")) {
+      $(this).removeAttr("checked");
+    } else {
+      $(this).attr("checked", "checked");
+    }
+
+    $(this).parent().toggleClass("completed");
+    localStorage.setItem("listItems", $("#list-items").html());
+  });
+
+  $(document).on("click", ".remove", function () {
+    $(this).parent().remove();
+    localStorage.setItem("listItems", $("#list-items").html());
+
+    // $(this).parent().slideUp()("fast", function () {
+    //   // Animation complete.
+    // });
   });
 });
--- project-4/index.js
+++ project-4/index.js
@@ -1,5 +1,43 @@
+/** @format */
 $(document).ready(function() {
-    // YOUR CODE HERE!
-});
+  $.ajax({
+    url: "https://one-month-jquery.s3.us-west-2.amazonaws.com/apartments.json",
+    dataType: "json",
+    success: function(response) {
+      $.each(response.apartments, function(i, apartment) {
+        var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+        var listing = "<a href='#' id=" + apartment.id + " class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>";
+          $(".apartments").append(listing);
+      }); 
+    },
+    error: function(error) {
+      console.log(error);
+    }
+   });
+ 
+   $(".filter").click(function() {
+    $(".filter").removeClass("active");
+    $(this).addClass("active");
+    var city = $(this).attr("id");
+    $(".listings").show();
+    if (city !== "all") {
+      $(".listings").not("." + city).css("display", "none");
+    }
+  });
 
+$(document).on("click",".listings",function(){
+  var id = $(this).attr("id")
+  $.ajax({
+    url: "https://one-month-jquery.s3.us-west-2.amazonaws.com/apartments.json",
+    dataType: "json",
+    success: function(response) {
+      $.each(response.apartments, function(i, apartment) {
+      
+    },
+    error: function(error) {
+      console.log(error);
+    }
+   });
+})
 
+ });
